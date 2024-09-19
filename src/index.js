72dab31d2ded3271c123a5135f61f0cb1c9c40f7
SSuryaRao <suryaraosodi@gmail.com>
2024-09-19 12:47:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home_page from './landing_page/home/Home_page';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './landing_page/Header';
 import Navbar from './landing_page/Navbar';
+import Home_page from './landing_page/home/Home_page';
 import DashPage from './landing_page/dashboard/DashPage';
 import Hero1 from './landing_page/earthquake/Hero1';
 import Hero2 from './landing_page/flood/Hero2';
 import Hero3 from './landing_page/landslide/Hero3';
 import Hero4 from './landing_page/helpline/Hero4';
 import NotFound from './landing_page/NotFound';
-import App from './App';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
       <Header />
       <Navbar />
       <Routes>
